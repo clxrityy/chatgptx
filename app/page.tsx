9dfaf24bb2625d03109ac87949ee92a21ee2216f
clxrityy <clxrityxyz@gmail.com>
2023-03-23 12:55:59 -0400

    chatgptx ;]

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ function HomePage() {
     return (
         <div className='flex flex-col items-center justify-center h-screen px-2 text-white'>
             <h1 className='text-5xl font-bold mb-20'>
-                ChatGPT
+                ChatGPTx
             </h1>
             <div className='flex space-x-2 text-center'>
                  {/* 1 */}
