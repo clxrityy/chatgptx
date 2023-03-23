9a1b80c65a01ff91e2451e61c7236083f4af2bf1
clxrityy <clxrityxyz@gmail.com>
2023-03-23 12:47:55 -0400

    fix auth?

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
+  site: process.env.NEXTAUTH_URL,
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
