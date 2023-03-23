818309f0b5b25b774873826455906ac48870ed45
clxrityy <clxrityxyz@gmail.com>
2023-03-23 19:34:08 -0400

    height

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { BoltIcon, ExclamationTriangleIcon, SunIcon } from '@heroicons/react/24/
 
 function HomePage() {
     return (
-        <div className='flex flex-col items-center justify-center h-screen px-2 text-white'>
+        <div className='flex flex-col items-center justify-center h-auto md:h-screen px-2 text-white'>
             <h1 className='text-5xl font-bold mb-20'>
                 ChatGPTx
             </h1>
