78ae8f136c33b2a29cb1eb47afcc2a8021611a7b
clxrityy <clxrityxyz@gmail.com>
2023-03-24 15:02:19 -0400

    model selection button

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,6 +1,8 @@
 'use client';
 import useSWR from 'swr';
 import Select from 'react-select';
+import { useState } from 'react';
+import { CogIcon } from '@heroicons/react/24/solid';
 
 const fetchModels = () => fetch('/api/getModels').then(res => res.json())
 
@@ -9,11 +11,25 @@ export default function ModelSelection() {
     const { data: models, isLoading } = useSWR('models', fetchModels);
     const { data: model, mutate: setModel } = useSWR('model', {
         fallbackData: 'text-davinci-003'
-    })
+    });
+
+    const [selected, setSelected] = useState(false);
 
     return (
         <div className='mt-2'>
-            <Select
+            {!selected && (
+                <button
+                    onClick={() => setSelected(true)}
+                    className='rounded-md mt-2 bg-[#8290e6] hover:bg-[#5966c0] ease-out duration-150 text-white text-center flex flex-row items-center justify-center space-x-1 px-1 py-1'
+                >
+                    <CogIcon className='text-white h-4 w-4' />
+                    <p className='hidden md:block'>
+                        Model Settings
+                    </p>
+                </button>
+            )}
+            {selected && (
+                <Select
                 
                 className='mt-2'
                 options={models?.modelOptions}
@@ -27,6 +43,7 @@ export default function ModelSelection() {
                 }}
                 onChange={(e) => setModel(e.value)}
             />
+            )}
         </div>
     );
 }
