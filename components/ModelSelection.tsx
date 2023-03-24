a4fe73fca9f447c5dffa08d097b2de3a17c9bd62
clxrityy <clxrityxyz@gmail.com>
2023-03-24 15:07:20 -0400

    button fix

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -18,15 +18,15 @@ export default function ModelSelection() {
     return (
         <div className='mt-2'>
             {!selected && (
-                <button
+                <div
                     onClick={() => setSelected(true)}
-                    className='rounded-md mt-2 bg-[#8290e6] hover:bg-[#5966c0] ease-out duration-150 text-white text-center flex flex-row items-center justify-center space-x-1 px-1 py-1'
+                    className='rounded-md mt-2 bg-[#8290e6] hover:bg-[#5966c0] ease-out duration-200 text-center flex items-center justify-center space-x-2 text-gray-300 transition-all px-5 py-3 cursor-pointer'
                 >
                     <CogIcon className='text-white h-4 w-4' />
                     <p className='hidden md:block'>
                         Model Settings
                     </p>
-                </button>
+                </div>
             )}
             {selected && (
                 <Select
